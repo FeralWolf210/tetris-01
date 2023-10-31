9fc0ea14a222a896f914dca322ec2ccf4722867b
Rxyy <lynardic69@gmail.com>
2023-10-31 20:09:16 +0700

    add js: j block properties

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const width = 10;
+
+    // J Block and Rotation
+    const jBlock = [
+        [1, 2, width + 1, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2, width * 2 + 1],
+        [width, width * 2, width * 2 + 1, width * 2 + 2],
+    ];
+
+    // S Block and Rotation
+});
