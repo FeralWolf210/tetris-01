f269ee6fd7232913da6c719690fa9c2a91c644f5
denTan10 <denny.tantoso10@gmail.com>
2023-11-21 14:20:13 +0700

    add tasklist

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ we put our blood and soul here.. pls like:>
 # role
 1. Lionel   : HTML 
 2. Bryan    : CSS
-3. denny    : JS
-# What we need to do
-1. Start from design
+3. Denny    : JS
+
+# List To Do
+# 21 nov 2023 (Today)
+  1. Halaman Script.js sudah ada 1 variable *jBlock*, lanjutkan dari zblock - iBlock
+  2. Setelah semua bentuk sudah terisi di masing" array variable, kumpulkan ke 5 variable (j - iBlock) ke dalam 1 variable baru dengan cara buat satu variable baru (hint googling : how to collect an array in array variable)
--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const width = 10;
+  const width = 10;
 
-    // J Block and Rotation
-    const jBlock = [
-        [1, 2, width + 1, width * 2 + 1],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2, width * 2 + 1],
-        [width, width * 2, width * 2 + 1, width * 2 + 2],
-    ];
+  // j Block and Rotation
+  const jBlock = [
+    [1, 2, width + 1, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2, width * 2 + 1],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
 
-    // S Block and Rotation
+  // z Block and Rotation
+  const zBlock = [[], [], [], []];
+
+  // t Block and Rotation
+  const tBlock = [[], [], [], []];
+
+  // s Block and Rotation
+  const sBlock = [[], [], [], []];
+
+  // i Block and Rotation
+  const iBlock = [[], [], [], []];
+
+  // Put the 5 variable on the top with 1 variables (theTetromnioes)
+  const theTetromnioes = [];
 });
